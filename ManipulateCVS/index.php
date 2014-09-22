b8470641384db2f868de15649590666bb9537cd6
Rubens Basso <rubens.tavares.basso@gmail.com>
2014-09-21 21:16:49 -0500

    Chances_21Sep2014

--- ManipulateCVS/index.php
+++ ManipulateCVS/index.php
@@ -4,8 +4,6 @@
 	//Manipulate Excel/CVS
 	ini_set("auto_detect_line_endings", TRUE); //Detect the end of the file to avoid issues in MAC OS
 	$excelFile = new manipulateCVS();
-	//$excelFile->readCVS("teste.cvs");
-	//$excelFile->readCVSMapping("teste.cvs");
 	$excelFile->readCVSMappingAndQuery("ImportFilev01.csv");
 	ini_set('auto_detect_line_endings',FALSE);
 	
